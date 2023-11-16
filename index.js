150cfbc909fe5bae0038e5f232d9b99cd4ab5e21
Samuel <89140281+Nenossauro@users.noreply.github.com>
2023-11-16 16:17:15 -0300

    criado index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.imgdiv img').click(function(){
+        $('.login-enzo').toggle();
+    })
+    $('.carrinho').click(function(){
+        document.location.href='cardapio.html';
+    })
+})
